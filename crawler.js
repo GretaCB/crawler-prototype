8bef42c6c4a15037c14421b1c943f9c4cc0ca3a5
GretaCB <carolbhansen@gmail.com>
2019-12-08 21:43:55 -0600

    Actually start crawling HTML and properly handle Promise

--- crawler.js
+++ crawler.js
@@ -12,29 +12,63 @@ async function crawl(url, level) {
 	if (level == 0) return;
 	let urls = {};
 	let record;
-	console.log('about to call getCrawlRecord');
-	try {
-		record = await redis.getCrawlRecord(id);
-	} catch(err){
-		console.log('caught error when getting record in crawl');
-		throw err;
-	}
+
+	// TODO: Check dns cache to see if the url has been hit before 
+	// TODO: Check request cache to see if page contents already stored
+	const request_cached = await redis.getRequestCache(url);
+	console.log('request_cache: ' + request_cached);
 	
-	console.log(record);
+	// Make request to URL if not cached
+	if (utils.isnull(request_cached)) {
+		urls = await _getPage(url).then(_getLinks);
+
+		// Store links in cache
+		try {
+			await redis.setRequestCache(url, JSON.stringify(urls));
+		} catch(err){
+			console.log('caught error when requesting cache');
+			throw err;
+		}
+	} else urls = request_cached;
+
+	console.log('urls: ' + JSON.stringify(urls));
 	
-	record = {};
-	record.urls_crawled = total_crawled;
-	console.log(record);
-	// Update results in record
+	// record = {};
+	// record.urls_crawled = total_crawled;
+	// console.log(record);
+	// // Update results in record
 	await redis.setCrawlRecord(id, JSON.stringify(record));
 	return await crawl(url, level-1);	
 }
+
+async function _getLinks($) {
+	let urls = {};
+
+	// Grab all links on page
+	const links = $('a');
+
+	$(links).each(function(i, link){
+		const hrefString = ($(link).attr('href'));
+		
+		// Only grab absolute links
+		var pat = /^https?:\/\//i;
+		if (pat.test(hrefString)) {
+   			urls[i] = hrefString;
+		}
+ 	});
+
+ 	return urls;
+}
+
+async function _getPage(url) {
+	const result = await axios.get(url);
+	return cheerio.load(result.data);
+};
 
 // receive message from master process
 process.on('message', async (message) => {  
 	id = message.id;
 
-	console.log('about to run crawl for the first time');
 	try {
 		await crawl(message.seedurl, message.levels);
 	} catch(err){
@@ -51,7 +85,6 @@ process.on('message', async (message) => {
 });
 
 process.on('error', (err) => {
-	console.log('in error handler!!!');
+  console.log('in error handler!!!');
   console.log(err);
-  //process.exit(err);
 });
